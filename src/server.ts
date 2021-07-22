a10af97d2f5e0d3b315e74b861167fbf1f2636c4
Adson B de Souza <adsonb87@gmail.com>
2021-07-22 13:38:43 -0300

    Servidor iniciado

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import  express  from "express";
+const app = express();
+app.get("/",function(req,res){
+    res.json(
+        {
+            "message:":"connected"
+        }
+    );
+});
+app.listen(8080,function(){
+    console.log("servidor funcionando na pota 8080");
+});
+
