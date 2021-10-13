96a58a71e24ff405b2fc0830b0abe8fac50f8623
Anglesson A <anglesson@hotmail.com>
2021-10-13 11:31:33 -0300

    feat: ajuste para chamada do bind

--- src/controllers/OvertimeController.ts
+++ src/controllers/OvertimeController.ts
@@ -4,10 +4,11 @@ import { User } from "../models/User";
 import { IOvertimeRepository } from "../repository/IOvertimeRepository";
 
 export class OvertimeController {
-    private overtimeRepository: IOvertimeRepository;
+    public overtimeRepository: IOvertimeRepository;
 
     constructor(overtimeRepository: IOvertimeRepository) {
         this.overtimeRepository = overtimeRepository;
+        this.create = this.create.bind(this);
     }
 
     async create(req: Request, res: Response) {
@@ -15,10 +16,9 @@ export class OvertimeController {
             let user = new User("Anglesson", "anglesson@email.com.br");
             const {date, start_time, end_time, description} = req.body;
             const overtime = new Overtime(date, start_time, end_time, description, user);
+            const overtimeSaved = this.overtimeRepository.save(overtime);
 
-            this.overtimeRepository.save(overtime);
-
-            //res.json({ data: overtimeSaved});
+            res.json({ data: overtimeSaved});
         } catch(e) {
             res.json({error: `${e}`});
         }
