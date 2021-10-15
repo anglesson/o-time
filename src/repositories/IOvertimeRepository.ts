3ba6ee6abe82043e28bb90b07a423c85e51a47ef
Anglesson Araujo <anglesson@hotmail.com>
2021-10-14 23:29:22 -0300

    refactor: remoção dos antigos repositorios

--- src/repositories/IOvertimeRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Overtime } from "../entities/Overtime";
-import { User } from "../entities/User";
-
-export interface IOvertimeRepository {
-    save(overtime: Overtime): Promise<Overtime>;
-    //getOvertimesByUser(user: User): Array<Overtime>;
-    //getAllOvertimes(): Array<Overtime>;
-}
