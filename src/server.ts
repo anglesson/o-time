5b677217e3db63e0575055302bb29a7005e6d256
Anglesson Araujo <anglesson@hotmail.com>
2021-10-15 16:20:13 -0300

    feat: criação das rotas all, show and update

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,10 @@ const userController = new UserController();
 const app = express();
 app.use(express.json());
 
+app.get('/api/overtime', overtimeController.getAll);
+app.get('/api/overtime/:id', overtimeController.show);
 app.post('/api/overtime', overtimeController.create);
+app.put('/api/overtime/:id', overtimeController.update);
 app.post('/api/user', userController.create);
 
 const port = 3003;
